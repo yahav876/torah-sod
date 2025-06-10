e141b1201bb9297a50807cc0c2e7e6d521fe165f
yahavhorev <yahav.horev@vidaa.com>
2025-06-10 10:19:40 +0300

    update scriot foir multi replica

---
+++ update-multi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Updating code from git repository..."
+git pull
+
+echo "Stopping and removing all containers..."
+docker compose -f docker-compose.multi-replica.yml down
+docker compose -f docker-compose.aws.yml down
+docker stop torah-sod-torah-search-1 || true
+docker rm torah-sod-torah-search-1 || true
+
+echo "Building and starting new containers with no cache to apply all changes..."
+docker compose -f docker-compose.multi-replica.yml build --no-cache
+docker compose -f docker-compose.multi-replica.yml up -d
+
+echo "Update complete! Running containers:"
+docker ps
